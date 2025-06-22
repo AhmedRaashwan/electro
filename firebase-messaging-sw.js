29a77e3f73e157ae3957f30df64285275dda14cd
Ahmed Rashwan <AhmedRaashwan@gmail.com>
2025-06-22 15:40:23 +0300

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,22 +1,22 @@
-// importScripts("https://www.gstatic.com/firebasejs/10.12.1/firebase-app-compat.js");
-// importScripts("https://www.gstatic.com/firebasejs/10.12.1/firebase-messaging-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/10.12.1/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/10.12.1/firebase-messaging-compat.js");
 
-// firebase.initializeApp({
-//   apiKey: "AIzaSyA2abvzifuMGk-tQEO8uymc08i8NvMAHwI",
-//   authDomain: "notify-raa.firebaseapp.com",
-//   projectId: "notify-raa",
-//   messagingSenderId: "767633213120",
-//   appId: "1:767633213120:web:0a19231dc1f04e3ced0f25",
-// });
+firebase.initializeApp({
+  apiKey: "AIzaSyA2abvzifuMGk-tQEO8uymc08i8NvMAHwI",
+  authDomain: "notify-raa.firebaseapp.com",
+  projectId: "notify-raa",
+  messagingSenderId: "767633213120",
+  appId: "1:767633213120:web:0a19231dc1f04e3ced0f25",
+});
 
-// const messaging = firebase.messaging();
+const messaging = firebase.messaging();
 
-// messaging.onBackgroundMessage(function(payload) {
-//   console.log("📦 Background message received:", payload);
-//   const { title, body } = payload.notification;
+messaging.onBackgroundMessage(function(payload) {
+  console.log("📦 Background message received:", payload);
+  const { title, body } = payload.notification;
 
-//   self.registration.showNotification(title, {
-//     body,
-//     icon: '/icon.png' // Optional
-//   }); 
-// });
+  self.registration.showNotification(title, {
+    body,
+    icon: '/icon.png' // Optional
+  }); 
+});
